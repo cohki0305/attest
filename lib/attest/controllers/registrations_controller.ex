c7b2e30604030c2d4d05ed6d7513f4d95ee00add
cohki0305 <cohki0305@gmail.com>
2017-10-06 11:55:42 +0900

    refactor registration controller by defining controller in attest

---
+++ lib/attest/controllers/registrations_controller.ex
@@ -0,0 +1,22 @@
+defmodule Attest.Contoller.RegistrationsContoller do
+  use AttestWeb, :controller
+
+  def new(schema, conn) do
+    changeset = struct(schema) |> schema.changeset
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(schema, conn, params, redirect_path \\ "/") do
+    changeset = struct(schema) |> schema.changeset(params)
+    case Attest.Registration.create_resource(changeset) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "ようこそ" <> changeset.params["email"])
+        |> redirect(to: redirect_path)
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "アカウントを作成できませんでした")
+        |> render("new.html", changeset: changeset)
+    end
+  end
+end
