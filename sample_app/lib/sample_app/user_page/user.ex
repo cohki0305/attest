387f9d0575948df1011f6fbc65a655a438cb4c91
cohki0305 <cohki0305@gmail.com>
2017-10-06 09:55:39 +0900

    generate user’s schema and registration controller command: mix attest.gen.schema UserPage User users

---
+++ sample_app/lib/sample_app/user_page/user.ex
@@ -0,0 +1,24 @@
+defmodule SampleApp.UserPage.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias SampleApp.UserPage.User
+
+  schema "users" do
+    field :email, :string
+    field :password, :string, virtual: true
+    field :crypted_password, :string
+    field :verified, :boolean
+
+    timestamps()
+  end
+
+  def changeset(%User{} = user, attrs \\ %{}) do
+    user
+    |> cast(attrs, [:email, :password, :verified])
+    |> change(%{verified: false})
+    |> validate_required([:email, :password, :verified])
+    |> update_change(:email, &String.downcase/1)
+    |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+  end
+end
