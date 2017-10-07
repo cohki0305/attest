ffdfdda6029dd468851842d431ea5dc72ae36f03
cohki0305 <cohki0305@gmail.com>
2017-10-07 09:57:20 +0900

    fix mix.exs for publishing

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Attest.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/cohki0305/attest"
     ]
   end
 
@@ -18,6 +21,18 @@ defmodule Attest.Mixfile do
     ]
   end
 
+  def description do
+    "user authentication library"
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["cohki0305"],
+      links: %{"GitHub" => "https://github.com/cohki0305/attest"},
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
@@ -25,6 +40,7 @@ defmodule Attest.Mixfile do
       {:phoenix_ecto, "~> 3.2", optional: true},
       {:comeonin, "~> 4.0"},
       {:bcrypt_elixir, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
