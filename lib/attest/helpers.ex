4fde416c694054afb278937503219f79857b4196
cohki0305 <cohki0305@gmail.com>
2017-10-10 21:09:49 +0900

    warn that not defining resources in config

--- lib/attest/helpers.ex
+++ lib/attest/helpers.ex
@@ -2,23 +2,26 @@ defmodule Attest.Helpers do
   alias Attest.Config
   import Plug.Conn
 
+  case Config.resources do
+    nil -> IO.warn "Define resources in config.ex"
+    _ ->
+      Config.resources |> Enum.each(fn {context, schema} ->
 
-  Config.resources |> Enum.each(fn {context, schema} ->
+        context_str = context |> Atom.to_string
+        context_module = context_str |> Phoenix.Naming.camelize
 
-    context_str = context |> Atom.to_string
-    context_module = context_str |> Phoenix.Naming.camelize
+        schema_str = schema |> Atom.to_string
+        schema_module = schema_str |> Phoenix.Naming.camelize
 
-    schema_str = schema |> Atom.to_string
-    schema_module = schema_str |> Phoenix.Naming.camelize
+        def unquote(:"current_#{schema_str}")(conn, repo \\ Attest.Config.repo) do
+          id = get_session(conn, unquote(:"current_#{schema_str}"))
+          if id, do: unquote(:"#{Config.base}.#{context_module}.#{schema_module}") |> repo.get(id)
+        end
 
-    def unquote(:"current_#{schema_str}")(conn, repo \\ Attest.Config.repo) do
-      id = get_session(conn, unquote(:"current_#{schema_str}"))
-      if id, do: unquote(:"#{Config.base}.#{context_module}.#{schema_module}") |> repo.get(id)
-    end
-
-    def unquote(:"#{schema_str}_logged_in?")(conn) do
-      account = unquote(:"current_#{schema_str}")(conn)
-      !!account
-    end
-  end)
+        def unquote(:"#{schema_str}_logged_in?")(conn) do
+          account = unquote(:"current_#{schema_str}")(conn)
+          !!account
+        end
+      end)
+  end
 end
